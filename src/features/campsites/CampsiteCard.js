d6b99560b3f973f73455dca50e61865819159e91
Dan Trauten <dan.trauten@gmail.com>
2023-03-20 14:58:03 -0500

    Exercise: Destructuring

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,18 +1,19 @@
-import  { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-const CampsiteCard = (props) => {
-	return (
-		<Card>
-			<CardImg
-				width="100%"
-				src={props.campsite.image}
-				alt={props.campsite.name}
-			/>
-			<CardImgOverlay>
-				<CardTitle>{props.campsite.name}</CardTitle>
-			</CardImgOverlay>
-		</Card>
-	);
-}
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
+    return (
+        <Card>
+            <CardImg
+                width="100%"
+                src={image}
+                alt={name}
+            />
+            <CardImgOverlay>
+                <CardTitle>{name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+};
 
 export default CampsiteCard;
