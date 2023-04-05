f8004b2e6e5765d2958fea024e56f651b927ca90
Dan Trauten <dan.trauten@gmail.com>
2023-04-05 13:17:37 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,16 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
@@ -10,4 +22,4 @@ export const selectCampsiteById = (id) => {
 
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
-}
+};
